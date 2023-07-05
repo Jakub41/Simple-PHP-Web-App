a1a6878b53bb6b559d996fce116cf6c2ea030186
FBanitz <francois.banitz@gmail.com>
2023-07-05 11:46:23 +0200

    added CI steps names

--- php/connection.php
+++ php/connection.php
@@ -6,6 +6,7 @@ $dbname = "mono"; // DB name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
