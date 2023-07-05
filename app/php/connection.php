b525ab25567def9838b28cd7e26a8bb468436f91
FBanitz <francois.banitz@gmail.com>
2023-07-05 15:02:21 +0200

    feat: added docker image pipeline

--- app/php/connection.php
+++ app/php/connection.php
@@ -1,5 +1,5 @@
  <?php
-$servername = "localhost"; // Server name where the app has the DB
+$servername = "spwa-mariadb"; // Server name where the app has the DB
 $username = "root"; // user of the DB 
 $password = "root"; // password of the DB
 $dbname = "mono"; // DB name 
--- app/php/head.php
+++ app/php/head.php
@@ -5,10 +5,10 @@
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
 
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="js/jquary.js"></script>
+<script src="../js/jquary.js"></script>
 
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -16,7 +16,7 @@
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
 <!-- Language Files -->
-<script src="js/language.js"></script>
+<script src="../js/language.js"></script>
 
 </head>
 
